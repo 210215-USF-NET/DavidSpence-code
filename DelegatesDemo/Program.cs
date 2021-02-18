bbf1e9b8d68cb346ed65bc1c8aa0b2875eb7f813
davidcspence3 <davidcspence3@gmail.com>
2021-02-18 16:21:01 -0500

    first version of DelegatesDemo

---
+++ DelegatesDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DelegatesDemo
+{
+    class Program
+    {
+
+        public delegate int Calc(int i, int j);
+
+        public static int Add(int x, int y)
+        {
+            int sum = x + y;
+            return sum;
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            int product = x * y;
+            return product;
+        }
+        static void Main(string[] args)
+        {
+            
+            Calc c1 = new Calc(Add);
+            Calc c2 = new Calc(Multiply);
+
+            Console.WriteLine(c1(3,3));
+            Console.WriteLine(c2(3,3));
+        }
+    }
+}
